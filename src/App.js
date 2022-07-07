44c2ca80645012ac91e27a0e32ffe175d66aefab
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-06 23:52:56 -0300

    adding title and logo
    title: Nike Store
    logo: Nike logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './img/nike.png';
 import './App.css';
 
 function App() {
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Nike Store</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
