04eee15cc7f44d7d20be9fb9210211d7e53f2617
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-12 00:10:43 -0300

    Updating NavBar component
    logo added
    navbar added
    slogan added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './img/nike.png';
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Nike Store</h1>
-        <div>
-          <a
-            className="App-link"
-            href="https://github.com/NahuelGenchi/nikestore"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Project repository
-          </a>
-          <span> | Nahuel Genchi</span>
-        </div>
-        
-      </header>
-    </div>
+    <header>
+      <NavBar />
+    </header>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import '../scss/NavBar.scss';
+import nike from '../img/nike.png'
+
+const NavBar = function () {
+  return (
+    <header className='header'>
+      <div className='navBigContainer'>
+        <div className='navContainer'>
+          <div className='navLogo'>
+            <h1>NIKE</h1>
+          </div>
+          <nav>
+            <ul>
+              <li><a href='#a'>Home</a></li>
+              <li><a href='#b'>Features</a></li>
+              <li><a href='#c'>Shopping</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className='logoContainer'>
+        <img src={nike} alt='Nike logo' className='headerLogoMain' />
+        <span className='headerSlogan'>JUST DO IT.</span>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
