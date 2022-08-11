af38c522d90d06cdece05fe2e60cb051ed99bde7
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-11 00:44:50 -0300

    improving cart design and content

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 
 import "./Cart.scss";
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
+  let total = 0;
+
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price;
+  }
+  console.log(total);
 
   return(
     <div className="cartSection">
-      <h1 className="cart-title">Cart</h1>
-      {cart.map((product) => {
+      <h1 className="cart-title">Cart <span>({cart.length})</span></h1>
+      {cart.map((product, index) => {
         return(
-          <h2>{product.title}</h2>
+          <div className="cart-product" key={index}>
+            <div className="cp-img-container">
+              <img src={product.img} alt="" />
+            </div>
+            <div className="cp-info">
+              <span className="cpi-title">{product.title}</span>
+              <p className="cpi-price">{product.price}</p>
+              <p className="cpi-delete">Delete</p>
+            </div>
+          </div>
         );
       })}
+      <p className="cart-total">Total: ${total}</p>
     </div>
   );
 };
