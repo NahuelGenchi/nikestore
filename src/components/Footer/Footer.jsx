ea116d166e35839da6a727e41915171529492129
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-31 19:00:33 -0300

    adding responsiveness to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,20 +3,22 @@ import { LinkIcon } from "../index";
 import "./Footer.scss";
 
 const Footer = () => {
-  return(
+  return (
     <footer className="footer">
       <div className="f-container">
         <div className="fc-links">
-          <LinkIcon icon="logo-github" link="https://github.com/NahuelGenchi"/>
-          <LinkIcon icon="logo-linkedin" link="https://www.linkedin.com/in/nahuel-genchi"/>
-          <LinkIcon icon="mail" link="mailto:nahuelgenchi@gmail.com"/>
+          <LinkIcon icon="logo-github" link="https://github.com/NahuelGenchi" />
+          <LinkIcon icon="logo-linkedin" link="https://www.linkedin.com/in/nahuel-genchi" />
+          <LinkIcon icon="mail" link="mailto:nahuelgenchi@gmail.com" />
         </div>
-        <div className="fc-location">
-          <ion-icon name="location"></ion-icon>
-          <span>Argentina</span>
-        </div>
-        <div className="fc-copyright">
-          <span>© 2022 Nahuel Genchi</span>
+        <div className="fc-info">
+          <div className="fc-location">
+            <ion-icon name="location"></ion-icon>
+            <span>Argentina</span>
+          </div>
+          <div className="fc-copyright">
+            <span>© 2022 Nahuel Genchi</span>
+          </div>
         </div>
       </div>
     </footer>
