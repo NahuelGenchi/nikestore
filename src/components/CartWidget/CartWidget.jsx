b36f466e42f67005582bd15e27e94dfe9ec001e6
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-09-02 23:27:27 -0300

    making cartwidget visible

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -7,7 +7,7 @@ const CartWidget = function(props) {
   const { cart, totalQuantity } = useContext(CartContext);
 
   return (
-      <div className={`cart-widget ${props.className}`} style={{display: cart.length < 1 && "none"}} open={cart.length > 0 ? true : false}>
+      <div className={`cart-widget ${props.className}`} /*style={{display: cart.length < 1 && "none"}}*/ open={cart.length > 0 ? true : false}>
         <Link to={`/cart`}><ion-icon name="bag-outline"></ion-icon></Link>
         <span>{totalQuantity}</span>
       </div>
